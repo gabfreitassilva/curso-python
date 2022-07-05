94b43aca7dd2e46dcb4f614efe0307654c74ccc1
Gabriel Freitas <contatofreitas.gab@hotmail.com>
2022-07-05 13:30:20 -0300

    primeiros exercicios

---
+++ mundo um/desafio003.py
@@ -0,0 +1,6 @@
+numero01 = int(input('Digite um numero inteiro: '))
+numero02 = int(input('Digite outro numero inteiro: '))
+
+soma = numero01 + numero02
+
+print('A soma dos dois numero e {}'.format(soma))
