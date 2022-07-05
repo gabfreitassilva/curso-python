b531db1d934ae3c3d56121d0d554d683479dc39a
Gabriel Freitas <contatofreitas.gab@hotmail.com>
2022-07-05 15:42:59 -0300

    melhorando formatacao

--- mundo um/desafio008.py
+++ mundo um/desafio008.py
@@ -3,4 +3,4 @@ metros = float(input('Digite um valor em metros: '))
 centimetros = metros * 100
 milimetros = metros * 1000
 
-print('O valor em centimetros é {} e em milimetros é {}'.format(centimetros, milimetros))
+print('O valor em centimetros é {:.0f} e em milimetros é {:.0f}'.format(centimetros, milimetros))
